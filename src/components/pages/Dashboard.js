2f861bc4ade7770e715c7b41d1447cd05d35dacc
ghoshlaltu <ghoshlaltu216@gmail.com>
2023-09-02 13:08:17 +0530

    dashboard

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BASE_URL from '../../config';
+import { Link } from "react-router-dom";
 
 function DashboardContent() {
+
+
+
+  const apiUrl = BASE_URL;
+  
+  const [loading, setLoading] = useState(false);
+  const [myData, setData] = useState([]);
+  const storedToken = localStorage.getItem('token');
+  const token = 'Bearer '+storedToken;
+
+  
+  useEffect(() => {
+    //console.log(data);
+    fetchData();
+  }, []);
+  
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const data = { 
+        // Add your request payload here 
+      };
+      const response = await axios.post(apiUrl+'api/site-dashboard-count-info', data, {
+        headers: {
+          Authorization: token
+        }
+      });
+      setData(response.data);
+      // SuccessNotify(response.data.message);
+      setLoading(false); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //call all data
     return (
       <>
+      {loading ? (
+           <div className="row">
+              <div className="loader-container">
+                <div className="spinner"></div>
+              </div>
+          </div>
+        ) : (
         <main className="dash-content">
             <div className="container-fluid">
               <div className="row dash-row">
-                <div className="col-xl-6">
+                
+                <div className="col-xl-4">
                   <div className="stats stats-primary">
                     <h3 className="stats-title"> Stories </h3>
                     <div className="stats-content">
@@ -14,7 +60,7 @@ function DashboardContent() {
                         <i className="fas fa-user" />
                       </div>
                       <div className="stats-data">
-                        <div className="stats-number">114</div>
+                      <Link to="/stories"><div className="stats-number text-white">{(myData.data) ? myData.data.storyCount : 0}</div></Link>
                         <div className="stats-change">
                           {/* <span className="stats-percentage">+25%</span>
                           <span className="stats-timeframe">from last month</span> */}
@@ -23,15 +69,32 @@ function DashboardContent() {
                     </div>
                   </div>
                 </div>
-                <div className="col-xl-6">
+                <div className="col-xl-4">
                   <div className="stats stats-success ">
                     <h3 className="stats-title"> Films </h3>
+                    <div className="stats-content">
+                      <div className="stats-icon">
+                        <i className="fas fa-video" />
+                      </div>
+                      <div className="stats-data">
+                      <Link to="/films"><div className="stats-number text-white">{(myData.data) ? myData.data.filmsCount : 0}</div></Link>
+                        <div className="stats-change">
+                          {/* <span className="stats-percentage">+17.5%</span>
+                          <span className="stats-timeframe">from last month</span> */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-4">
+                  <div className="stats stats-secondary ">
+                    <h3 className="stats-title"> Enquery Messages </h3>
                     <div className="stats-content">
                       <div className="stats-icon">
                         <i className="fas fa-phone" />
                       </div>
                       <div className="stats-data">
-                        <div className="stats-number">25</div>
+                      <Link to="/contact-messages"><div className="stats-number text-white">{(myData.data) ? myData.data.msgCount : 0}</div></Link>
                         <div className="stats-change">
                           {/* <span className="stats-percentage">+17.5%</span>
                           <span className="stats-timeframe">from last month</span> */}
@@ -119,7 +182,8 @@ function DashboardContent() {
                 </div>
               </div> */}
             </div>
-          </main>
+          </main>  
+          )}
       </>
     );
   }
